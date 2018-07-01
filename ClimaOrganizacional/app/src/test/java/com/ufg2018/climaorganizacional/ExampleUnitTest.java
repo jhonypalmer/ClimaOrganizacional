3bd4068aeb79e1d35b0c74c060469dd0623a0be2
Marconi Braga <marconi.braga@siedos.com.br>
2018-07-01 17:39:32 -0300

    Subindo Base Projeto

---
+++ ClimaOrganizacional/app/src/test/java/com/ufg2018/climaorganizacional/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.ufg2018.climaorganizacional;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
